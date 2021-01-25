56c7c9377a39dda01a84f29d2e7d1c2faaded6db
dortoo <dorto@live.com>
2021-01-25 03:01:00 -0300

    part 2.1

---
+++ src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Details, Img } from './styles';
+
+import img13 from '../../images/13.png';
+
+export default function Banner() {
+  return (
+    <Container>
+      <Details>
+
+      </Details>
+      <Img source={img13} />
+    </Container>
+  );
+}
---
+++ src/components/Banner/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #6882BB;
+  margin: 25px 16px;
+  border-radius: 8px;
+  height: 100px;
+`;
+
+export const Details = styled.View``;
+
+export const Img = styled.Image``;
